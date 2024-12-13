8993f7829e2b3dda800102a9cd7f7dfe7f44739d
unknown <ag_antro@hotmail.com>
2024-12-13 18:42:16 -0300

    added background and h1 to proyects section

--- src/components/proyects/proyects.jsx
+++ src/components/proyects/proyects.jsx
@@ -4,7 +4,19 @@ const Proyects = () => {
   return (
     <>
       <section className="proyects">
-        <div></div>
+        <div className="proyects-container">
+          <h1 className="proyects-h1">PROYECTS</h1>
+        </div>
+        <div className="proyects-video-container">
+          <video
+            autoPlay
+            muted
+            loop
+            id="video"
+            src="keyboard1.mp4"
+            type="keyboard"
+          ></video>
+        </div>
       </section>
     </>
   );
