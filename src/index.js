0537323045fce1500b257468448a3e5af29b3100
unknown <ag_antro@hotmail.com>
2025-01-10 11:10:27 -0300

    corrected browser router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from 'react-router';
 import '../src/scss/index.scss';
 import '../src/scss/mediaqueries.scss'
 import '../src/scss/animations.scss'
 import App from './App';
-
-import "./i18n"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +12,7 @@ root.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+
   </React.StrictMode>
 );
 
